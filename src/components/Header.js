c2349902251e013397a8d2f2e44b4e2f51a13560
Prakhar1123 <mishra.prakhar309@gmail.com>
2023-10-31 20:32:19 +0530

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
 import './Header.css';
-import prakharImage from './prakhar.jpg';
+import prakharImage from './prakharImage.jpg'
 
 const Header = () => {
-  const [text, setText] = useState("Full Stack Developer");
-
-  useEffect(() => {
-    const animationInterval = setInterval(() => {
-      if (text === "Full Stack Developer") {
-        setText("Full Stack Developer");
-      } else {
-        setText("UI Developer");
-      }
-    }, 3000);
-
-    return () => {
-      clearInterval(animationInterval);
-    };
-  }, []);
-
   return (
-    <header id="header" className="header">
-      <div className="left-content">
-        <div className="fixed-text">
-          <p>Hi, I am Prakhar Kumar</p>
-          <p>I am a</p>
-          <p className="animate-character">
-            <span className="full-stack-developer">{text}</span>
-          </p>
-          <p className='ps'>
-            I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning, I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.
-          </p>
-          <a 
-            href="https://drive.google.com/uc?export=download&id=1lhe9ECTmuHoQEC23LpUahtcBdD_KsI0P
-            " download="PrakharKumar_Resume.pdf" className="download-link button"
-          >
-            Download my resume
-          </a>
-        </div>
-        <div className="right-content">
-          <div className="rotation-axis-container">
-            {/* Create several rotating lines (electromagnetic lines) */}
-            {/* Add more lines as needed */}
+    <header id="header" className="header" role="banner">
+      <div className="content">
+        <div className="left-content">
+          <div className="fixed-text">
+            <h1>Hi, I am Prakhar Kumar</h1>
+            <p>I am a</p>
+            <p className="animate-character">
+              <span className="full-stack-developer">Full Stack Developer</span>
+            </p>
+            <p className='ps'>
+              I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning, I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.
+            </p>
+            <a
+              href="https://drive.google.com/uc?export=download&id=1lhe9ECTmuHoQEC23LpUahtcBdD_KsI0P"
+              download="PrakharKumar_Resume.pdf"
+              className="download-link button"
+              aria-label="Download my resume"
+            >
+              Download my resume
+            </a>
           </div>
-          <img src={prakharImage} alt="Prakhar Kumar" className="my-image" />
+          <img src={prakharImage} alt="Image of Prakhar Kumar" className="my-image" />
         </div>
       </div>
     </header>
