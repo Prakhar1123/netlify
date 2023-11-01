b36f30399af107d17c6b5dfd03cb14e8a64b5e7c
Prakhar1123 <71943892+Prakhar1123@users.noreply.github.com>
2023-11-02 00:02:25 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Skills.css';
 import img1 from './html.png';
 import img2 from './css.png';
-import img3 from './javascript.png'
+import img3 from './javascript.png';
 import img4 from './react.png';
 import img5 from './next.png';
 import img6 from './node.png';
@@ -10,6 +10,7 @@ import img7 from './express.png';
 import img8 from './python.png';
 import img9 from './mysql.png';
 import img10 from './java.png';
+
 const Skills = () => {
     return (
         <div id="skills" className="skills">
@@ -18,22 +19,21 @@ const Skills = () => {
                 <div className="front-end-skills">
                     <h2>Front-End Skills</h2>
                     <ul>
-                        <li><img src={img1}/>HTML</li>
-                        <li><img src={img2}/>CSS</li>
-                        <li><img src={img3}/>JavaScript</li>
-                        <li><img src={img4}/>ReactJS</li>
-                        <li><img src={img5}/>Next.js</li>
+                        <li><img src={img1} alt="HTML" />HTML</li>
+                        <li><img src={img2} alt="CSS" />CSS</li>
+                        <li><img src={img3} alt="JavaScript" />JavaScript</li>
+                        <li><img src={img4} alt="ReactJS" />ReactJS</li>
+                        <li><img src={img5} alt="Next.js" />Next.js</li>
                     </ul>
                 </div>
                 <div className="back-end-skills">
                     <h2>Back-End Skills</h2>
                     <ul>
-                        <li><img src={img6}/>Node Js</li>
-                        <li><img src={img7}/>Express Js</li>
-                        <li><img src={img8}/>Python</li>
-                        <li><img src={img9}/>SQL</li>
-                        <li><img src={img10}/>Java</li>
-                       
+                        <li><img src={img6} alt="Node Js" />Node Js</li>
+                        <li><img src={img7} alt="Express Js" />Express Js</li>
+                        <li><img src={img8} alt="Python" />Python</li>
+                        <li><img src={img9} alt="SQL" />SQL</li>
+                        <li><img src={img10} alt="Java" />Java</li>
                     </ul>
                 </div>
             </div>
